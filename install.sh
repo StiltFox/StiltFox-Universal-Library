f7b0791165c05947c5e74344657c1f8b43cf2b77
Cryos <cryos@stiltfox.com>
2023-02-15 10:14:42 -0500

    changing cmake files and install files to support find_package again

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ mkdir build
 cd build
 cmake .. -DSFSkipTesting=true
 cmake --build .
-cmake --install . --prefix ~/.local/dev-bin --config Release
+cmake --install . --prefix ~/.local/dev-bin
 cd ..
 mkdir buildWin
 cd buildWin
 cmake .. -DCMAKE_TOOLCHAIN_FILE=windows.cmake -DSFSkipTesting=true
 cmake --build .
-cmake --install . --prefix ~/.local/dev-win --config Release
+cmake --install . --prefix ~/.local/dev-win
 cd ..
